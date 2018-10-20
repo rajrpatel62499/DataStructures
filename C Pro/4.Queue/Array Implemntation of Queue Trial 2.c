250bfe88a2f76850557a0a5f7c8165552af444ed
rajrpatel <rajrpatel62499@gmail.com>
2018-10-20 12:32:10 +0530

    Update C Pro/4.Queue/Array Implemntation of Queue Trial 2.c

---
+++ C Pro/4.Queue/Array Implemntation of Queue Trial 2.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+struct Queue
+{
+    int front;
+    int rear;
+    int capacity;
+    int *array;
+};
+
+
+
+struct Queue* createQueue(int capacity)
+{
+    struct Queue *q1=(struct Queue*)malloc( sizeof(struct Queue));
+    q1->front=-1;
+    q1->rear=-1;
+    q1->capacity=capacity;
+    q1->array=(int*)malloc(capacity * sizeof(int));
+    return q1;
+}
+int isFull(struct Queue* q1)
+{
+   if((q1->rear+1)%q1->capacity==q1->front)
+        return 1;
+   else
+    return 0;
+}
+int isEmpty(struct Queue* q1)
+{
+    if(q1->rear==-1 && q1->front==-1)
+        return 1;
+    else
+        return 0;
+}
+struct Queue*  enQueue(struct Queue* q1,int data)
+{
+    if(isFull(q1))
+    {
+        printf("Queue is overflow...can't Enqueue data\n");
+    }else{
+   printf("Enqueuing....");
+    if(isEmpty(q1))
+    {
+        q1->front=q1->rear=0;
+    }else
+    {
+      q1->rear=(q1->rear+1)%q1->capacity;
+    }
+    q1->array[q1->rear]=data;
+
+    }
+  return q1;
+}
+struct Queue* deQueue(struct Queue* q1)
+{
+ if(isEmpty(q1))
+ {
+     printf("Queue is Empty...Can't Dequeue data");
+ }else
+ {
+    printf("Dequeueing...");
+     if(q1->front==q1->rear)
+     {
+         q1->front=q1->rear=-1;
+     }else
+     {
+         q1->front= ( (q1->front+1)%q1->capacity);
+     }
+ }
+ return q1;
+}
+int size(struct Queue* q1)
+{
+    return ( (q1->capacity+q1->rear-q1->front) % q1->capacity + 1);
+}
+void print(struct Queue* q1)
+{
+    int count=0,index;
+    int s=size(q1);
+    printf("Queue is: ");
+    if(isEmpty(q1))
+    {
+        printf("Queue is Empty Now...Contain garbage value:");
+
+    }
+    for(count=0;count<s;count++)
+    {
+        index= (q1->front +count)%q1->capacity;
+        printf("%d ",q1->array[index]);
+    }
+    printf("\n");
+}
+void main()
+{
+    struct Queue* q1=createQueue(5);
+   q1=enQueue(q1,1); print(q1);
+   q1=enQueue(q1,2); print(q1);
+   q1=enQueue(q1,3); print(q1);
+   q1=enQueue(q1,4); print(q1);
+   q1=enQueue(q1,5); print(q1);
+    printf("\n\n");
+    q1=deQueue(q1); print(q1);
+   q1=deQueue(q1); print(q1);
+   q1=deQueue(q1); print(q1);
+   q1=deQueue(q1); print(q1);
+   q1=deQueue(q1); print(q1);
+
+}
